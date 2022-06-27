05643aada4b06ece8c55a9f2e65a4061a2f5610d
Wiloux <54811449+Wiloux@users.noreply.github.com>
2022-06-27 18:36:02 +0200

    HDRP + graph

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,17 +6,26 @@ public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     public float strength;
+    public float rotationSpd;
     public List<GameObject> skins = new List<GameObject>();
     void Start()
     {
-        if (strength < 3)
+        if (strength < 1)
 		{
             skins[0].SetActive(true);
 		}
-		else
+		else if (strength < 2)
 		{
             skins[1].SetActive(true);
-        } ;
+        }
+        else if (strength < 3)
+        {
+            skins[2].SetActive(true);
+        }
+        else
+        {
+            skins[3].SetActive(true);
+        };
 
         transform.localScale = Vector3.one * strength;
     }
@@ -26,6 +35,6 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }

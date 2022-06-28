26487a7310ed5a606123962f1670f3252c9fec65
Nagdalam <chariou.benjamin@gmail.com>
2022-06-28 12:36:48 +0200

    Gravitational pull
    In the Gravitational Pull script

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -22,22 +22,7 @@ public class Enemy : MonoBehaviour
 
 
     void Start()
-    { if (strength <= 1)
-		{
-            skins[0].SetActive(true);
-		}
-		else if (strength > 1 && strength <= 2 )
-		{
-            skins[1].SetActive(true);
-        }
-        else if (strength > 2 && strength <= 3)
-        {
-            skins[2].SetActive(true);
-        }
-        else if(strength > 3)
-        {
-            skins[3].SetActive(true);
-        };
+    { 
         if (strength <= 1)
 		{
             skins[0].SetActive(true);
@@ -54,26 +39,8 @@ public class Enemy : MonoBehaviour
         {
             skins[3].SetActive(true);
         };
-
-    planetBody = GetComponent<Rigidbody>();
-        blackHoleTransform = FindObjectOfType<PlayerController>().transform;
-        transform.localScale = Vector3.one * strength;
-
-
     }
 
-        if (strength < 3)
-        {
-            skins[0].SetActive(true);
-        }
-        else
-        {
-            skins[1].SetActive(true);
-        }
-        planetBody = GetComponent<Rigidbody>();
-        blackHoleTransform = FindObjectOfType<PlayerController>().transform;
-        transform.localScale = Vector3.one * strength;
-    }
 
 
 
@@ -89,27 +56,8 @@ public class Enemy : MonoBehaviour
             transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, deathCurve.Evaluate(step));
             transform.position = Vector3.Lerp(transform.position, target.position, deathCurve.Evaluate(step));
 		}
-		  distanceToPlayer = Vector2.Distance(this.transform.position, transform.position);
-        if (distanceToPlayer <= influenceRange)
-        {
-            pullForce = (blackHoleTransform.position - this.transform.position).normalized / distanceToPlayer * pullIntensity;
-            planetBody.AddForce(pullForce, ForceMode.Force);
-        }
 
 
-		if (dead)
-        {
-            step +=  0.05f * Time.deltaTime;
-
-            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, deathCurve.Evaluate(step));
-            transform.position = Vector3.Lerp(transform.position, target.position, deathCurve.Evaluate(step));
-		}
-        distanceToPlayer = Vector2.Distance(this.transform.position, transform.position);
-        if (distanceToPlayer <= influenceRange)
-        {
-            pullForce = (blackHoleTransform.position - this.transform.position).normalized / distanceToPlayer * pullIntensity;
-            planetBody.AddForce(pullForce, ForceMode.Force);
-        }
     }
 
 }
---
+++ Assets/Scripts/GravitationnalPull.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitationnalPull : MonoBehaviour
+{
+    [SerializeField] private Transform pulledTarget;
+    [SerializeField] private Transform pullingObject;
+    private Rigidbody targetBody;
+    [SerializeField] private float influenceRange;
+    [SerializeField] private float intensity;
+    private float distanceToPlayer;
+    private Vector3 pullForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pulledTarget = this.transform;
+        targetBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(pulledTarget.position, pullingObject.position);
+        if(distanceToPlayer < influenceRange)
+        {
+            pullForce = (pullingObject.position - pulledTarget.position).normalized / distanceToPlayer * intensity;
+            targetBody.AddForce(pullForce);
+        }
+        else if (distanceToPlayer > influenceRange + influenceRange/2)
+        {
+            targetBody.velocity = Vector3.zero;
+        }
+    }
+}
